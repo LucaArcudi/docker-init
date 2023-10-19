d7c8c97875cd91b6a198b17cb53121def79ee263
LucaArcudi <arcudilu@gmail.com>
2023-10-19 16:48:35 +0200

    feat: add namespaces; add custom autoload

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,24 @@
 <?php
-phpinfo();
+
+declare(strict_types=1);
+
+// require_once __DIR__ . '/../App/PaymentGateway/Stripe/Transaction.php';
+// require_once __DIR__ . '/../App/PaymentGateway/Paddle/Transaction.php';
+
+spl_autoload_register(
+    function ($class) {
+        $path =  __DIR__ . '/../' . str_replace('\\', '/', $class) . '.php';
+        require_once $path;
+    }
+);
+
+use App\PaymentGateway\Paddle\Transaction as PaddleTransaction;
+use App\PaymentGateway\Stripe\Transaction as StripeTransaction;
+
+echo '<pre>';
+var_dump(new StripeTransaction(100));
+echo '</pre>';
+
+echo '<pre>';
+var_dump(new PaddleTransaction(1045));
+echo '</pre>';
